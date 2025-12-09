5b0ab172efadbb02446f88b9685040d0d828a0c2
maxbek97 <maxbek294@gmail.com>
2025-12-09 03:22:54 +0300

    Подготовил файлы для запихивания в докер. ТАм тестовый env файл лежит, но там другие данные надо будет вводить разумеется.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SberVolunteerAPI.Models;
 using SberVolunteerAPI.Services;
+using System;
 using System.Text;
 
 
@@ -12,11 +13,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<SberVolunteerContext>(options => options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
-        )
-);
+
+var cs = $"Server={Environment.GetEnvironmentVariable("DB_HOST")};" +
+         $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+         $"Database={Environment.GetEnvironmentVariable("DB_DATABASE")};" +
+         $"User={Environment.GetEnvironmentVariable("DB_USER")};" +
+         $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};";
+
+builder.Services.AddDbContext<SberVolunteerContext>(options =>
+    options.UseMySql(cs, ServerVersion.AutoDetect(cs)));
+
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<JWTService>();
 builder.Services.Configure<AuthSettings>(
@@ -55,6 +61,7 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // JWT auth
+
 builder.Services
     .AddAuthentication(options =>
     {
@@ -75,6 +82,12 @@ builder.Services
     });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SberVolunteerContext>();
+    db.Database.Migrate();
+}
 
 // Middleware
 if (app.Environment.IsDevelopment())
